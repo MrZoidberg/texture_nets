d628c2bda3ff6cbffdf64d864434bd5f359eb49e
Mikhail Merkulov <mikhailm@benders-Mac-mini-7.local>
2017-01-25 13:44:51 +0200

    original colors option

--- runtest.sh
+++ runtest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-while getopts "i:o:s:m:" opt; do
+while getopts "i:o:s:m:c:" opt; do
 case $opt in
 i) SOURCE="$OPTARG"
 ;;
@@ -9,6 +9,8 @@ o) OUTPUT="$OPTARG"
 s) SIZE="$OPTARG"
 ;;
 m) MODEL="$OPTARG"
+;;
+c) COLORS="$OPTARG"
 ;;
 *) echo "No reasonable options found!";;
 esac
@@ -20,12 +22,12 @@ FILES=${SOURCE}/*
 for f in $FILES
 do
 	filename=$(basename "$f")
-  	echo "Processing $filename file..."  
-  	convert ${f} -resize ${SIZE}x${SIZE}^ -gravity center -extent ${SIZE}x${SIZE} resize/${filename}
+  	echo "Processing $filename mifile..."  
+  	convert "${f}" -resize ${SIZE}x${SIZE}^ -gravity center -extent ${SIZE}x${SIZE} "resize/${filename}"
 done
 
-th testbatch.lua -input_path resize -model_t7 ${MODEL} -save_path ${OUTPUT}
+th testbatch.lua -input_path resize -model_t7 ${MODEL} -save_path ${OUTPUT} -original_colors ${COLORS}
 
-rm -rf resize/*
+rm -rf resize
 
 
--- testbatch.lua
+++ testbatch.lua
@@ -12,11 +12,20 @@ cmd:option('-image_size', 0, 'Resize input image to. Do not resize if 0.')
 cmd:option('-model_t7', '', 'Path to trained model.')
 cmd:option('-save_path', '', 'Path to save stylized image.')
 cmd:option('-cpu', false, 'use this flag to run on CPU')
+cmd:option('-original_colors', false, 'original colors')
 
 local params = cmd:parse(arg)
 
 -- Load model and set type
 local model = torch.load(params.model_t7)
+
+-- Combine the Y channel of the generated image and the UV channels of the
+-- content image to perform color-independent style transfer.
+function original_colors(content, generated)
+  local generated_y = image.rgb2yuv(generated)[{{1, 1}}]
+  local content_uv = image.rgb2yuv(content)[{{2, 3}}]
+  return image.yuv2rgb(torch.cat(generated_y, content_uv, 1))
+end
 
 if params.cpu then
   tp = 'torch.FloatTensor'
@@ -49,6 +58,11 @@ for file in lfs.dir(params.input_path) do
      local input = img:add_dummy()
      local stylized = model:forward(input:type(tp)):double()
      stylized = deprocess(stylized[1])
+
+     -- Maybe color correct.
+     if params.correct_color then
+      stylized = original_colors(img:double(), stylized:double())
+     end
 
      -- Save
      image.save(params.save_path .. '/' .. file, torch.clamp(stylized,0,1))
