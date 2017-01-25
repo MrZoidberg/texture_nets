49fbce2fe25156dc743a84aa693b2d4b1ff21bbe
Mikhail Merkulov <mikhailm@benders-Mac-mini-7.local>
2017-01-25 13:57:17 +0200

    color_transfer.py

--- runtest.sh
+++ runtest.sh
@@ -22,12 +22,26 @@ FILES=${SOURCE}/*
 for f in $FILES
 do
 	filename=$(basename "$f")
-  	echo "Processing $filename mifile..."  
+  	echo "Resizing $filename file..."  
   	convert "${f}" -resize ${SIZE}x${SIZE}^ -gravity center -extent ${SIZE}x${SIZE} "resize/${filename}"
 done
 
-th testbatch.lua -input_path resize -model_t7 ${MODEL} -save_path ${OUTPUT} -original_colors ${COLORS}
+if [[ "${COLORS}" == "true" ]]; then
+	th testbatch.lua -input_path resize -model_t7 ${MODEL} -save_path ${OUTPUT} -original_colors
+	
+else
+	mkdir -p output_colors
+	th testbatch.lua -input_path resize -model_t7 ${MODEL} -save_path output_colors
+	for f in output_colors
+	do
+		filename=$(basename "$f")
+	  	echo "Processing original colors $filename file..."  
+	  	echo python color_transfer.py resize/${filename} f ${OUTPUT}/${filename}
+	  	python color_transfer.py resize/${filename} f ${OUTPUT}/${filename}
+	done
+fi
 
+rm -rf output_colors
 rm -rf resize
 
 
