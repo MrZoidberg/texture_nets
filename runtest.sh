bc2b30143d9ce652d793997c84a1a4a6019d103d
Mikhail Merkulov <mikhailm@benders-Mac-mini-7.local>
2017-01-27 14:23:59 +0200

    fix problems in runtest script

--- runtest.sh
+++ runtest.sh
@@ -26,18 +26,17 @@ do
   	convert "${f}" -resize ${SIZE}x${SIZE}^ -gravity center -extent ${SIZE}x${SIZE} "resize/${filename}"
 done
 
-if [[ "${COLORS}" == "true" ]]; then
+if [[ "${COLORS}" != "true" ]]; then
 	th testbatch.lua -input_path resize -model_t7 ${MODEL} -save_path ${OUTPUT} -original_colors
 	
 else
 	mkdir -p output_colors
 	th testbatch.lua -input_path resize -model_t7 ${MODEL} -save_path output_colors
-	for f in output_colors
+	for f in output_colors/*
 	do
 		filename=$(basename "$f")
 	  	echo "Processing original colors $filename file..."  
-	  	echo python color_transfer.py resize/${filename} f ${OUTPUT}/${filename}
-	  	python color_transfer.py resize/${filename} f ${OUTPUT}/${filename}
+	  	python color_transfer.py "resize/${filename}" "${f}" "${OUTPUT}/${filename}"
 	done
 fi
 
